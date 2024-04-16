9bc0e0b46344d9747c40eba87319181351c03214
Manuel Tomé <manuel.tome@epitech.eu>
2024-04-16 12:09:58 +0200

    build(package): fix deps

--- src/commands/join/index.ts
+++ src/commands/join/index.ts
@@ -1,40 +1,40 @@
+import { joinVoiceChannel } from '@discordjs/voice'
 import {
   ChannelType,
   CommandInteraction,
   SlashCommandBuilder,
   VoiceChannel,
-} from "discord.js";
-import { joinVoiceChannel } from "@discordjs/voice";
-import { Command } from "@/types";
+} from 'discord.js'
+
+import { Command } from '@/types'
 
 const data = new SlashCommandBuilder()
-  .setName("join")
-  .setDescription("Join a voice channel")
+  .setName('join')
+  .setDescription('Join a voice channel')
   .addChannelOption((option) =>
     option
-      .setName("channel")
-      .setDescription("Channel to join")
+      .setName('channel')
+      .setDescription('Channel to join')
       .setRequired(true)
-      .addChannelTypes(ChannelType.GuildVoice)
-  );
+      .addChannelTypes(ChannelType.GuildVoice),
+  )
 
 const execute = async (interaction: CommandInteraction): Promise<void> => {
-  const channel = interaction.options.get("channel", true)
-    .channel as VoiceChannel;
-  const guildId = channel.guildId;
-  const adapterCreator = channel.guild.voiceAdapterCreator;
-
+  const channel = interaction.options.get('channel', true)
+    .channel as VoiceChannel
+  const guildId = channel.guildId
+  const adapterCreator = channel.guild.voiceAdapterCreator
 
   joinVoiceChannel({
     channelId: channel.id,
     guildId,
     adapterCreator,
-  });
+  })
 
   interaction.reply({
     content: `Joining Channel #${channel.id}`,
     ephemeral: true,
-  });
-};
+  })
+}
 
-export default { data, execute } as Command;
+export default { data, execute } as Command
